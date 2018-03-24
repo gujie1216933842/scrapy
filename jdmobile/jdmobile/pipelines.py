09a685d1a66982d35c553f46c3dd2eab082846e3
gujie1216933842 <gujientsy@163.com>
2018-03-24 20:56:28 +0800

    codebase

--- jdmobile/jdmobile/pipelines.py
+++ jdmobile/jdmobile/pipelines.py
@@ -10,13 +10,17 @@ import re
 
 class JdmobilePipeline(object):
     def process_item(self, item, spider):
-        conn = pymysql.connect(host="127.0.0.1", user="root", passwd="123", db="python_spider", charset="utf8")
 
+        conn = pymysql.connect(host="127.0.0.1", user="root", passwd="123", db="python_spider", charset="utf8")
+        #游标
+        cursor = conn.cursor()
         for i in range(0, len(item['title'])):
             title = item['title'][i]
+            print(title)
             title = re.sub('\'','',title)
             title = re.sub('\"','',title)
-            sql = " insert into jd_mobile (name,raw_add_time ) VALUES ( '" + title + "' ,now())"
-            conn.query(sql)
+            sql = " insert into jd_mobile (name,raw_add_time ) VALUES ( %s ,now())"
+            cursor.execute(sql,title)
+        cursor.close()
         conn.close()
         return item

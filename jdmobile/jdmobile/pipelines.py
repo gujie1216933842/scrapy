94f342f79dc41cf6358b902b4ced848b4ab293a4
gujie1216933842 <gujientsy@163.com>
2018-03-24 21:59:21 +0800

    spider

--- jdmobile/jdmobile/pipelines.py
+++ jdmobile/jdmobile/pipelines.py
@@ -10,26 +10,24 @@ import re
 
 class JdmobilePipeline(object):
     def process_item(self, item, spider):
-
-        # conn = pymysql.connect(host="127.0.0.1", user="root", passwd="123", db="python_spider", charset="utf8")
-        # #游标
-        # cursor = conn.cursor()
+        conn = pymysql.connect(host="127.0.0.1", user="root", passwd="123", db="python_spider", charset="utf8")
+        # 游标
+        cursor = conn.cursor()
         for i in range(0, len(item['title'])):
             title = item['title'][i]
             price = item['price'][i]
+            price = price[1:]
+
             comment_count = item['comment_count'][i]
+            comment_count = comment_count[:-3]
             author = item['author'][i]
             publicing_company = item['publicing_company'][i]
-            print(title)
-            print(price)
-            print(comment_count)
-            print(author)
-            print(publicing_company)
-            # title = re.sub('\'','',title)
-            # title = re.sub('\"','',title)
-            # sql = " insert into jd_mobile (name,raw_add_time ) VALUES ( %s ,now())"
-            # cursor.execute(sql,title)
-            # conn.commit()
-        # cursor.close()
-        # conn.close()
+            title = re.sub('\'', '', title)
+            title = re.sub('\"', '', title)
+            sql = " insert into jd_mobile (name,raw_add_time ) VALUES ( %s ,now())"
+            cursor.execute(sql, title)
+            conn.commit()
+        cursor.close()
+        conn.close()
         return item
+
